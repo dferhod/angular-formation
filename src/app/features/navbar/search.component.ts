bd958c919fa11a6583f524008c7baf8dbb42951a
RSamaium <sam@webcreative5.net>
2024-10-02 11:15:32 +0200

    autocomplete

--- src/app/features/navbar/search.component.ts
+++ src/app/features/navbar/search.component.ts
@@ -1,6 +1,7 @@
-import { NgIf } from "@angular/common";
+import { NgFor, NgIf } from "@angular/common";
 import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { AutoCompletePipe } from "../../shared/pipes/autocomplete.pipe";
 
 @Component({
     selector: 'app-search',
@@ -12,13 +13,19 @@ import { FormsModule } from "@angular/forms";
         <!-- <button (click)="search()" *ngIf="userName != ''">Rechercher</button> -->
 
        <ul>
-        @for (name of fistNames ; track $index) {
-            <li>{{ $index }} - {{ name }}</li>
+        @for (name of fistNames | autocomplete:userName ; track $index) {
+            <li>{{ name }}</li>
         }
+        @empty {
+            <p>Aucun nom trouvé</p>
+        }
+            <!-- <li *ngFor="let name of fistNames ; let i = index ; let isFirst = first">
+                {{ isFirst }} - {{ name }}
+            </li> -->
         </ul>
     `,
     standalone: true,
-    imports: [FormsModule, NgIf]
+    imports: [FormsModule, /*NgIf, NgFor*/ AutoCompletePipe]
 })
 export class SearchComponent {
     @Input() userName = ''
